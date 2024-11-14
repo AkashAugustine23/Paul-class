b2a4be5225cf995f779ea6f7164a0ada46ece825
AkashAugustine23 <110322025+AkashAugustine23@users.noreply.github.com>
2024-11-14 22:08:35 +0000

    push

--- euler-sum.py
+++ euler-sum.py
@@ -1,1 +1,9 @@
+main
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+limit=int(input("enter the limit:"))
+sum=0
 
+for i in range (1,limit)
+if i%num1 == 0 and i%num2 == 0
+sum=sum+i
