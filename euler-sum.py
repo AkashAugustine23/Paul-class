b683892d091962875ba44a4e7af8f1ba6a8e8a7b
AkashAugustine23 <110322025+AkashAugustine23@users.noreply.github.com>
2024-11-14 22:13:20 +0000

    commit

--- euler-sum.py
+++ euler-sum.py
@@ -1,10 +1,11 @@
+print("this is a program in python")
 num1=int(input("enter the first number:"))
 num2=int(input("enter the second number:"))
 limit=int(input("enter the limit:"))
 sum=0
 
-for i in range (1,limit)
-if i%num1 == 0 and i%num2 == 0
-sum=sum+i
+for i in range (1,limit):
+    if i%num1 == 0 and i%num2 == 0:
+        sum=sum+i
 
 print("the sum is : ", {sum})
